9d610bb698e1fd81e83646ac3e774515b7a8e9be
= <zivojin.davidovic2001@gmail.com>
2023-02-13 14:39:51 +0100

    registration

---
+++ js/User.js
@@ -0,0 +1,30 @@
+class User{
+    user_id = '';
+    username = '';
+    email = '';
+    password = '';
+    api_url = 'https://63ea3946811db3d7ef09a465.mockapi.io';
+
+    create(){
+        let data = {
+            username: this.username,
+            email: this.email,
+            password: this.password
+        }
+
+        data = JSON.stringify(data);
+
+        fetch(this.api_url + '/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+        .then(response => response.json())
+        .then(data => {
+            window.location.href = 'hexa.html';
+        })
+    }
+
+}
--- js/app.js
+++ js/app.js
@@ -6,3 +6,46 @@ document.querySelector('#closeModal').addEventListener('click', () => {
     document.querySelector('.custom-modal').style.display = 'none';
 });
 
+let config = {
+    'username': {
+        required: true,
+        minlength: 5,
+        maxLength: 50
+    },
+    'register_email': {
+        required: true,
+        email: true,
+        minlength: 5,
+        maxLength: 50
+    },
+    'register_password':{
+        required: true,
+        minlength: 7,
+        maxLength: 25,
+        matching: 'repeat_password'
+    },
+    'repeat_password': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'register_password'
+    }
+};
+
+let validator = new Validator(config, '#registration-form');
+
+document.querySelector('#registration-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    if(validator.validationPassed()){
+
+        let user = new User();
+        user.username = document.querySelector('#username').value;
+        user.email = document.querySelector('#register_email').value;
+        user.password = document.querySelector('#register_password').value;
+        user.create();
+
+    }else{
+        alert('Nije ok');
+    }
+});
