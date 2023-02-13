2a00a7cf2daa89f52d5c95e9f1617526ce238558
= <zivojin.davidovic2001@gmail.com>
2023-02-13 13:59:29 +0100

    modal registration

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.querySelector('#register').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'block';
+});
+
+document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('.custom-modal').style.display = 'none';
+});
+
